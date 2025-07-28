515644ef32f5d5fabf5e290963d3d80233e2a287
Colin Leong <122366389+cleong110@users.noreply.github.com>
2025-07-28 23:02:18 +0000

    datamunging scripts

--- sign_bibles_dataset/data_analysis/run_insightface_on_frame_folders.sh
+++ sign_bibles_dataset/data_analysis/run_insightface_on_frame_folders.sh
@@ -9,18 +9,15 @@ project_root="$(dirname "$(dirname "$script_dir")")"
 
 # Parse arguments
 dir_to_search="${1:-$project_root/downloads}"   # default if not given
-workers="${2:-4}"                               # default to 5 workers
-stone_workers="${3:-4}"                         # ...each of which has workers
+workers="${2:-4}"                               # default to 4
 
 # Debug printout (remove or comment out if undesired)
 echo "Project root: $project_root"
 echo "Directory to search: $dir_to_search"
 echo "Parallel workers: $workers"
-echo "Skintone workers: $stone_workers"
 
 # Run the command in parallel
 find "$dir_to_search" -type d -name "*_frames" |
   grep -v "animation" |
   parallel --progress -j "$workers" \
-    stone -i "{}" -o "{}"/"skintone" --n_workers "$stone_workers"
-    # stone -i "{}" -o "{}"/"skintone" -d --n_workers "$stone_workers" # outputting debug images causes trouble for other scripts
+    python count_faces_insightface.py "{}"
