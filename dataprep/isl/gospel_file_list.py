45fc4a3b46e7090f0c6f7d4e8dd24437dd96cf5e
Kavitha Raju <kavitharaju18@gmail.com>
2025-08-13 12:37:35 +0530

    Remove nextcloud import

--- dataprep/isl/gospel_file_list.py
+++ dataprep/isl/gospel_file_list.py
@@ -1,16 +1,16 @@
 from pathlib import Path
 
-from nextcloud_connect import NextCloud_connection
+# from nextcloud_connect import NextCloud_connection
 
-def list_videofile_inputs(path, count_start=0):
-	nxt_cld_conn = NextCloud_connection()
-	files = nxt_cld_conn.get_files(path)
-	i = count_start
-	for file in files:
-		if not file.endswith("/"):
-			i += 1
-			print(f"{i}\t{path}{file}")
-	return i
+# def list_videofile_inputs(path, count_start=0):
+# 	nxt_cld_conn = NextCloud_connection()
+# 	files = nxt_cld_conn.get_files(path)
+# 	i = count_start
+# 	for file in files:
+# 		if not file.endswith("/"):
+# 			i += 1
+# 			print(f"{i}\t{path}{file}")
+# 	return i
 
 def list_videofile_inputs_onmount(path, count_start=0):
 	files = [f for f in Path(path).iterdir() if f.is_file()]
