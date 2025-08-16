9930c7c5f0d9a4709440a391da1521085b651712
Kavitha Raju <kavitharaju18@gmail.com>
2025-08-16 12:08:23 +0530

    Try workdir instead of filemount

--- dataprep/isl/lambda/scripts/split_words.py
+++ dataprep/isl/lambda/scripts/split_words.py
@@ -2,12 +2,12 @@ import pathlib
 import sys
 
 def main():
-    input_path = pathlib.Path("/inputs/sample.txt")   # hardcoded for now
+    input_path = pathlib.Path("inputs/sample.txt")   # hardcoded for now
     text = input_path.read_text()
 
     words = text.split()
 
-    output_path = pathlib.Path("/outputs/words.txt")
+    output_path = pathlib.Path("outputs/words.txt")
     output_path.write_text("\n".join(words))
 
     print(f"Processed {len(words)} words. Output saved to {output_path}")
